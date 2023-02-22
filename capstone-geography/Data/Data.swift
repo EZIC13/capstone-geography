369fa57ce71be096450e69f3c0fca22d5c51f17b
Connor Klingenberg <cmklingtroop770@outlook.com>
2023-02-22 16:02:13 -0700

    Added JSON offline capability (for if the API is down when the first request is made). This is for demonstration purposes if the API is offline and there is no UserDefaults key active.

--- capstone-geography/Data/Data.swift
+++ capstone-geography/Data/Data.swift
@@ -42,6 +42,28 @@ class CountriesService: ObservableObject {
     init() {
         getCountries()
     }
+    
+    func load<T: Decodable>(_ filename: String) -> T {
+        let data: Data
+        
+        guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Could not find \(filename) in main bundle")
+        }
+        
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("Could not load \(filename) from main bundle: \(error)")
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            fatalError("Could not parse \(filename) as \(T.self): \(error)")
+        }
+    }
 
     func getCountries() {
         let url = URL(string: "http://ec2-44-232-173-177.us-west-2.compute.amazonaws.com:3000/countries")!
@@ -63,6 +85,11 @@ class CountriesService: ObservableObject {
                         }
                     } catch {
                         print("Error decoding saved countries: \(error)")
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.countries = self.load("countries.json")
+                        print("Retrieved saved countries from JSON")
                     }
                 }
                 return
